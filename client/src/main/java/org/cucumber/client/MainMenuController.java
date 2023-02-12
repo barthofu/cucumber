d4c3b82c6b563885cdadba13663bb6c17ceff479
Noé Favier <noe.favier@outlook.com>
2023-02-12 20:15:36 +0100

    feat(GUI): added buttons on main menu

--- client/src/main/java/org/cucumber/client/MainMenuController.java
+++ client/src/main/java/org/cucumber/client/MainMenuController.java
@@ -1,4 +1,23 @@
 package org.cucumber.client;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import org.cucumber.common.so.LoggerStatus;
+import org.cucumber.common.utils.Logger;
+
+import java.io.IOException;
+
 public class MainMenuController {
+
+    @FXML
+    protected void onQuitButton(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    protected void onFavButton(ActionEvent event) {
+        Logger.log(LoggerStatus.INFO, "fav");
+    }
+
+
 }
